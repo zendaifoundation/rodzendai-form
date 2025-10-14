7c94c6ee0c98aac57ac09d67645d6d460585affb
WutthinanK <developer1@npappdesign.com>
2025-10-14 13:25:20 +0700

    feat: Add forms for patient and companion information, including address details and validation

---
+++ lib/presentation/register_to_claim_your_rights/views/form_companion_info.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rodzendai_form/core/constants/app_colors.dart';
+import 'package:rodzendai_form/core/constants/app_text_styles.dart';
+import 'package:rodzendai_form/core/utils/input_formatters.dart';
+import 'package:rodzendai_form/core/utils/validators.dart';
+import 'package:rodzendai_form/presentation/register/interfaces/contact_relatio_type.dart';
+import 'package:rodzendai_form/presentation/register/widgets/form_header.dart';
+import 'package:rodzendai_form/presentation/register_to_claim_your_rights/providers/register_to_claim_your_rights_provider.dart';
+import 'package:rodzendai_form/widgets/base_card_container.dart';
+import 'package:rodzendai_form/widgets/radio_group_field.dart';
+import 'package:rodzendai_form/widgets/text_form_field_customer.dart';
+
+//ข้อมูลรายละเอียดผู้ติดตาม
+class FormCompanionInfo extends StatelessWidget {
+  const FormCompanionInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<RegisterToClaimYourRightsProvider>(
+      builder: (context, registerProvider, child) {
+        return BaseCardContainer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 16,
+            children: [
+              FormHeaderWidget(title: 'รายละเอียดผู้ติดตาม'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Checkbox(
+                    value: registerProvider.patientInfoForCompanion,
+                    onChanged: (value) {
+                      registerProvider.usePatientInfoForCompanion(
+                        value ?? false,
+                      );
+                    },
+                    activeColor: AppColors.primary,
+                    checkColor: AppColors.white,
+                    side: BorderSide(color: AppColors.textLighter, width: 2),
+                  ),
+                  //Text('ใช้ข้อมูลผู้แจ้ง/ผู้ติดต่อ'),
+                  Text('ใข้ข้อมูลผู้ป่วย', style: AppTextStyles.regular),
+                ],
+              ),
+              TextFormFielddCustom(
+                label: 'หมายเลขบัตรประชาชน',
+                hintText: 'เลขบัตรประชาชน 13 หลัก',
+                controller: registerProvider.companionIdCardController,
+                isRequired: true,
+                inputFormatters: InputFormatters.citizenId,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  return Validators.validateIdCardNumber(value);
+                },
+              ),
+              Row(
+                spacing: 16,
+                children: [
+                  Expanded(
+                    child: TextFormFielddCustom(
+                      label: 'ชื่อ',
+                      hintText: 'ชื่อ',
+                      controller: registerProvider.companionFirstNameController,
+                      isRequired: true,
+                      validator: Validators.required('กรุณากรอกข้อมูล'),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormFielddCustom(
+                      label: 'นามสกุล',
+                      hintText: 'นามสกุล',
+                      controller: registerProvider.companionLastNameController,
+                      isRequired: true,
+                      validator: Validators.required('กรุณากรอกข้อมูล'),
+                    ),
+                  ),
+                ],
+              ),
+              RadioGroupField<ContactRelationType>(
+                key: ValueKey(registerProvider.companionRelationSelected),
+                label: 'ความสัมพันธ์',
+                isRequired: true,
+                value: registerProvider.companionRelationSelected,
+                options: ContactRelationType.values
+                    .map(
+                      (relation) =>
+                          RadioOption(value: relation, label: relation.value),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  registerProvider.setCompanionRelationSelected(value);
+                },
+                validator: (value) {
+                  if (value == null) {
+                    return 'กรุณาเลือกความสัมพันธ์';
+                  }
+                  return null;
+                },
+              ),
+              TextFormFielddCustom(
+                label: 'เบอร์โทรติดต่อ',
+                hintText: 'เบอร์โทรติดต่อ',
+                controller: registerProvider.companionPhoneController,
+                isRequired: true,
+                inputFormatters: InputFormatters.phone,
+                keyboardType: TextInputType.number,
+                validator: Validators.required('กรุณากรอกข้อมูล'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/presentation/register_to_claim_your_rights/views/form_patient_info.dart
+++ lib/presentation/register_to_claim_your_rights/views/form_patient_info.dart
@@ -7,10 +7,15 @@ import 'package:rodzendai_form/core/extensions/text_editing_controller_extension
 import 'package:rodzendai_form/core/utils/input_formatters.dart';
 import 'package:rodzendai_form/core/utils/validators.dart';
 import 'package:rodzendai_form/models/patient_record_model.dart';
+import 'package:rodzendai_form/presentation/register/interfaces/patient_type.dart';
+import 'package:rodzendai_form/presentation/register/interfaces/transport_ability.dart';
+import 'package:rodzendai_form/presentation/register/widgets/form_header.dart';
 import 'package:rodzendai_form/presentation/register_to_claim_your_rights/blocs/data_patient_bloc/data_patient_bloc.dart';
 import 'package:rodzendai_form/presentation/register_to_claim_your_rights/providers/register_to_claim_your_rights_provider.dart';
+import 'package:rodzendai_form/widgets/base_card_container.dart';
 import 'package:rodzendai_form/widgets/button_custom.dart';
 import 'package:rodzendai_form/widgets/dialog/app_dialogs.dart';
+import 'package:rodzendai_form/widgets/radio_group_field.dart';
 import 'package:rodzendai_form/widgets/text_form_field_customer.dart';
 
 class FormPatientInfo extends StatelessWidget {
@@ -20,101 +25,182 @@ class FormPatientInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<RegisterToClaimYourRightsProvider>(
       builder: (context, registerProvider, child) {
-        return Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              spacing: 16,
-              children: [
-                Expanded(
-                  child: TextFormFielddCustom(
-                    label: 'หมายเลขบัตรประชาชน',
-                    controller: registerProvider.patientIdCardController,
-                    isRequired: true,
-                    inputFormatters: InputFormatters.citizenId,
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      return Validators.validateIdCardNumber(value);
+        return BaseCardContainer(
+          child: Column(
+            spacing: 16,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              FormHeaderWidget(title: 'รายละเอียดผู้ป่วย'),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                spacing: 16,
+                children: [
+                  Expanded(
+                    child: TextFormFielddCustom(
+                      label: 'หมายเลขบัตรประชาชน',
+                      hintText: 'เลขบัตรประชาชน 13 หลัก',
+                      controller: registerProvider.patientIdCardController,
+                      isRequired: true,
+                      inputFormatters: InputFormatters.citizenId,
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        return Validators.validateIdCardNumber(value);
+                      },
+                    ),
+                  ),
+                  BlocBuilder<DataPatientBloc, DataPatientState>(
+                    builder: (context, state) {
+                      return SizedBox(
+                        height: 48,
+                        child: ButtonCustom(
+                          // key: ValueKey(
+                          //   registerProvider.patientIdCardController.text.trim(),
+                          // ),
+                          text: 'ตรวจสอบข้อมูล',
+                          isLoading: state is DataPatientLoading,
+                          onPressed:
+                              registerProvider
+                                          .patientIdCardController
+                                          .textOrNull ==
+                                      null ||
+                                  registerProvider
+                                          .patientIdCardController
+                                          .text
+                                          .length !=
+                                      13
+                              ? null
+                              : () async {
+                                  String idCardNumber = registerProvider
+                                      .patientIdCardController
+                                      .text
+                                      .trim();
+                                  log('Checking ID Card: $idCardNumber');
+                                  List<PatientRecordModel> dataPatients = [];
+                                  if (state is DataPatientLoaded) {
+                                    dataPatients = state.dataPatients;
+                                  }
+
+                                  if (dataPatients.isEmpty) {
+                                    await AppDialogs.error(
+                                      context,
+                                      message:
+                                          'ไม่สามารถเชื่อมต่อข้อมูลผู้ป่วยได้',
+                                    );
+                                  }
+
+                                  bool isFound = dataPatients
+                                      .where(
+                                        (element) =>
+                                            element.idCardNumber ==
+                                            idCardNumber,
+                                      )
+                                      .isNotEmpty;
+
+                                  if (!isFound) {
+                                    return await AppDialogs.error(
+                                      context,
+                                      title: 'ไม่สามารถลงทะเบียนได้',
+                                      message:
+                                          'ขออภัยในความไม่สะดวก\n หมายเลขประจำตัวประชาชน $idCardNumber\nไม่อยู่ในกลุ่มเป้าหมายที่ให้บริการในขณะนี้',
+                                    );
+                                  }
+
+                                  await AppDialogs.success(
+                                    context,
+                                    title: 'สามารถลงทะเบียนได้',
+                                    message:
+                                        'รหัสประจำตัวประชาชน $idCardNumber\n สามารถลงทะเบียนได้\nกรุณากรอกข้อมูลให้ครบถ้วนและถูกต้อง',
+                                  );
+
+                                  //เนื่องจากรหัสประจำตัวประชาชน 1101800389482 ได้ใช้บริการครบ 3 ครั้งตามกำหนดแล้ว
+                                },
+                        ),
+                      );
                     },
                   ),
-                ),
-                BlocBuilder<DataPatientBloc, DataPatientState>(
-                  builder: (context, state) {
-                    if (state is DataPatientLoaded) {
-                      log(
-                        'FormPatientInfo BlocBuilder rebuild: state=${state.dataPatients.length}',
-                      );
-                    }
-                    return SizedBox(
-                      height: 48,
-                      child: ButtonCustom(
-                        // key: ValueKey(
-                        //   registerProvider.patientIdCardController.text.trim(),
-                        // ),
-                        text: 'ตรวจสอบข้อมูล',
-                        isLoading: state is DataPatientLoading,
-                        onPressed:
-                            registerProvider
-                                        .patientIdCardController
-                                        .textOrNull ==
-                                    null ||
-                                registerProvider
-                                        .patientIdCardController
-                                        .text
-                                        .length !=
-                                    13
-                            ? null
-                            : () async {
-                                String idCardNumber = registerProvider
-                                    .patientIdCardController
-                                    .text
-                                    .trim();
-                                log('Checking ID Card: $idCardNumber');
-                                List<PatientRecordModel> dataPatients = [];
-                                if (state is DataPatientLoaded) {
-                                  dataPatients = state.dataPatients;
-                                }
+                ],
+              ),
+              Row(
+                spacing: 16,
+                children: [
+                  Expanded(
+                    child: TextFormFielddCustom(
+                      label: 'ชื่อ',
+                      hintText: 'ชื่อ',
+                      isRequired: true,
+                      controller: registerProvider.patientFirstNameController,
+                      validator: Validators.required('กรุณากรอกข้อมูล'),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormFielddCustom(
+                      label: 'นามสกุล',
+                      hintText: 'นามสกุล',
+                      isRequired: true,
+                      controller: registerProvider.patientLastNameController,
+                      validator: Validators.required('กรุณากรอกข้อมูล'),
+                    ),
+                  ),
+                ],
+              ),
+              TextFormFielddCustom(
+                label: 'เบอร์โทรติดต่อ',
+                hintText: 'เบอร์โทรติดต่อ',
+                isRequired: true,
+                controller: registerProvider.patientPhoneController,
+                keyboardType: TextInputType.phone,
+                inputFormatters: InputFormatters.phone,
+                validator: Validators.required('กรุณากรอกข้อมูล'),
+              ),
 
-                                if (dataPatients.isEmpty) {
-                                  await AppDialogs.error(
-                                    context,
-                                    message:
-                                        'ไม่สามารถเชื่อมต่อข้อมูลผู้ป่วยได้',
-                                  );
-                                }
+              TextFormFielddCustom(
+                label: 'Line ID (ถ้ามี)',
+                isRequired: false,
+                controller: registerProvider.patientLineIdController,
+              ),
 
-                                bool isFound = dataPatients
-                                    .where(
-                                      (element) =>
-                                          element.idCardNumber == idCardNumber,
-                                    )
-                                    .isNotEmpty;
+              RadioGroupField<PatientType>(
+                key: ValueKey(registerProvider.patientTypeSelected),
+                label: 'ประเภทผู้ป่วย',
+                isRequired: true,
+                value: registerProvider.patientTypeSelected,
+                options: PatientType.values
+                    .map((type) => RadioOption(value: type, label: type.value))
+                    .toList(),
+                onChanged: (value) {
+                  registerProvider.setPatientTypeSelected(value);
+                },
+                validator: (value) {
+                  if (value == null) {
+                    return 'กรุณาเลือกประเภทผู้ป่วย';
+                  }
+                  return null;
+                },
+              ),
 
-                                if (!isFound) {
-                                  return await AppDialogs.error(
-                                    context,
-                                    title: 'ไม่สามารถลงทะเบียนได้',
-                                    message:
-                                        'ขออภัยในความไม่สะดวก\n หมายเลขประจำตัวประชาชน $idCardNumber\nไม่อยู่ในกลุ่มเป้าหมายที่ให้บริการในขณะนี้',
-                                  );
-                                }
-
-                                await AppDialogs.success(
-                                  context,
-                                  title: 'สามารถลงทะเบียนได้',
-                                  message:
-                                      'รหัสประจำตัวประชาชน $idCardNumber\n สามารถลงทะเบียนได้\nกรุณากรอกข้อมูลให้ครบถ้วนและถูกต้อง',
-                                );
-
-                                //เนื่องจากรหัสประจำตัวประชาชน 1101800389482 ได้ใช้บริการครบ 3 ครั้งตามกำหนดแล้ว
-                              },
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
+              RadioGroupField<TransportAbility>(
+                key: ValueKey(registerProvider.transportAbilitySelected),
+                label: 'ความสามารถในการเดินทาง',
+                isRequired: true,
+                value: registerProvider.transportAbilitySelected,
+                options: TransportAbility.values
+                    .map(
+                      (ability) =>
+                          RadioOption(value: ability, label: ability.value),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  registerProvider.setTransportAbilitySelected(value);
+                },
+                validator: (value) {
+                  if (value == null) {
+                    return 'กรุณาเลือกความสามารถในการเดินทาง';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
         );
       },
     );
