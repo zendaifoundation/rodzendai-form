1bf6eb445c0bad28f73b0396b4eaa0db5e134aaf
WutthinanK <developer1@npappdesign.com>
2026-04-02 21:55:48 +0700

    chore: Update Flutter SDK version to 3.41.0 and bump package version to 1.0.19+162; update asset cache and dependencies

--- lib/presentation/splash/views/register_status_list.dart
+++ lib/presentation/splash/views/register_status_list.dart
@@ -184,9 +184,11 @@ class RegisterStatusList extends StatelessWidget {
       case '3':
         return 'ยกเลิก';
       case '4':
-        return 'รอดำเนินการ';
+        //return 'รอดำเนินการ';
+        return 'รอเจ้าหน้าที่ติดต่อกลับ';
       case '5':
-        return 'กำลังดำเนินการ';
+        //return 'กำลังดำเนินการ';
+        return 'จองคิวรถแล้ว';
       default:
         return '-';
     }
